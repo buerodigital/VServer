7d16128616473ff786dc63ce944c7f67c934df08
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-19 23:39:42 +0200

    Update 01_landing_create.sh

--- 01_landing_create.sh
+++ 01_landing_create.sh
@@ -8,11 +8,11 @@ clear
 echo -e "${YEL}=== Installing Landingpage "$DOMAIN" ===${NC}"
 
 echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
-mkdir $WORKFOLDER/volumes/00_landing_html
+mkdir $WORKFOLDER/volumes/01_landing_html
 echo -e "${GRA}=== Done ===${NC}"
 
 echo -e "\n${GRA}=== Creating YAML File for Landing Page and starting Container ===${NC}"
-cp $WORKFOLDER/01_landing_compose.yml $WORKFOLDER/01_landing_compose.yml
+cp /opt/VServer/01_landing_compose.yml $WORKFOLDER/01_landing_compose.yml
 sed -i -e "s|WORKFOLDER|$WORKFOLDER|g" $WORKFOLDER/01_landing_compose.yml
 sed -i -e "s|DOMAIN|$DOMAIN|g" $WORKFOLDER/01_landing_compose.yml
 sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WORKFOLDER/01_landing_compose.yml
