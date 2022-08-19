70c9fa872511a14984347adfae99eda18fdedb16
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-19 11:34:37 +0200

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,17 +1,65 @@
 #!/bin/bash
-                                                                                                                                                                                                                   
-clear                                                                                                                                                                                                              
-                                                                                                                                                                                                                   
-echo -e "=== Creating Folders for Bind-Volumes ==="
-                                                                                                                                                                                                                   
-mkdir /opt/apps.ideenrocker.com/vol_vhost
-mkdir /opt/apps.ideenrocker.com/vol_certs
-mkdir /opt/apps.ideenrocker.com/vol_conf
-mkdir /opt/apps.ideenrocker.com/vol_dhparam
-mkdir /opt/apps.ideenrocker.com/vol_html
-                                                                                                                                                                                                                   
-echo -e "=== Creating Proxy Network ==="
-                                                                                                                                                                                                                   
+
+# Bitte anpassen
+DOMAIN="apps.ideenrocker.com"
+SIAB_USER
+
+
+# Farbedefinitionen
+NC='\033[31;0m\'       # no colors or formatting
+RED='\033[0;31;1m\'    # print text in bold red
+PUR='\033[0;35;1m\'    # print text in bold purple
+YEL='\033[0;33;1m\'    # print text in bold Yellow
+GRA='\033[0;37;1m\'    # print text in bold Gray
+
+
+
+# Hinweis auf Löschung aller Daten
+clear
+
+echo -e "${RED}=== Achtung!!! Alle Daten werden gelöscht!!! ===${NC}"
+echo -e "${RED}=== Ggf. mit CTRL + C abbrechen ===${NC}"
+
+COUNT=5
+while [ $COUNT -gt 0 ]; do
+  echo "${NC}$COUNT"
+  let COUNT=COUNT-1
+done
+
+echo "${NC}GO!"
+
+mkdir /opt/$DOMAIN
+mkdir /opt/$DOMAIN/volumes
+
+
+
+# Installation Proxy
+clear
+echo -e "${YEL}=== Installing NGINX-Proxy "$DOMAIN" ===${NC}"
+
+echo -e "\n${GRA}=== Creating Bind-Volumes Folders ===${NC}"
+mkdir /opt/$DOMAIN/volumes/proxy_certs
+mkdir /opt/$DOMAIN/volumes/proxy_conf
+mkdir /opt/$DOMAIN/volumes/proxy_vhost
+mkdir /opt/$DOMAIN/volumes/proxy_dhparam
+mkdir /opt/$DOMAIN/volumes/proxy_html
+echo -e "${GRA}=== Done ===${NC}"
+
+echo -e "\n${GRA}=== Creating Proxy Network ===${NC}"
 docker network create --driver bridge net_proxy
-                                                                                                                                                                                                                   
-echo -e "=== Done ==="
+echo -e "${GRA}=== Done ===${NC}"
+
+echo -e "\n${GRA}=== Creating YAML File for Porxy and starting Container ===${NC}"
+cp ./apps_compose.yml /opt/$DOMAIN/apps_compose.yml
+sed -i -e 's/DOMAIN/'"$DOMAIN"'/g' /opt/$DOMAIN/apps_compose.yml
+docker-compose -f /opt/$DOMAIN/apps_compose.yml up -d
+echo -e "${GRA}=== Done ===${NC}"
+
+
+
+
+
+
+
+
+
