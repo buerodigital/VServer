da25461f1fc9a9b21177ab2e77ecaecfee20de93
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-19 21:44:39 +0200

    Update include.sh

--- include.sh
+++ include.sh
@@ -1,3 +1,23 @@
-#!/bin/bash
 
-DOMAIN=apps.ideenrocker.com
+# Variablendeklaration - Bitte anpassen
+
+# Allgemein
+WORKFOLDER="/opt/docker"
+DOMAIN="apps.ideenrocker.com"
+LETSENCRYPTEMAIL="info@ideenrocker.com"
+
+#Shell in a Box
+SIABUSER="guest"
+SIABPASSWORD="xyz"
+
+# OnlyOffice
+JWTSECRET=caioe
+
+
+# Farbedefinitionen
+
+NC='\033[31;0m'       # no colors or formatting
+RED='\033[0;31;1m'    # print text in bold red
+PUR='\033[0;35;1m'    # print text in bold purple
+YEL='\033[0;33;1m'    # print text in bold Yellow
+GRA='\033[0;37;1m'    # print text in bold Gray
