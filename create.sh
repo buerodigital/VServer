6c1085f8aeb41bce2423d41aa8ab5a5cdc88aa7b
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-19 11:46:01 +0200

    Update create.sh

--- create.sh
+++ create.sh
@@ -2,15 +2,17 @@
 
 # Bitte anpassen
 DOMAIN="apps.ideenrocker.com"
-SIAB_USER
+SIABUSER="guest"
+SIABPASSWORD="xyz"
+LETSENCRYPTEMAIL="info@ideenrocker.com"
 
 
 # Farbedefinitionen
-NC='\033[31;0m\'       # no colors or formatting
-RED='\033[0;31;1m\'    # print text in bold red
-PUR='\033[0;35;1m\'    # print text in bold purple
-YEL='\033[0;33;1m\'    # print text in bold Yellow
-GRA='\033[0;37;1m\'    # print text in bold Gray
+NC='\033[31;0m'       # no colors or formatting
+RED='\033[0;31;1m'    # print text in bold red
+PUR='\033[0;35;1m'    # print text in bold purple
+YEL='\033[0;33;1m'    # print text in bold Yellow
+GRA='\033[0;37;1m'    # print text in bold Gray
 
 
 
@@ -27,6 +29,11 @@ while [ $COUNT -gt 0 ]; do
 done
 
 echo "${NC}GO!"
+
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+docker system prune -a
 
 mkdir /opt/$DOMAIN
 mkdir /opt/$DOMAIN/volumes
@@ -49,7 +56,7 @@ echo -e "\n${GRA}=== Creating Proxy Network ===${NC}"
 docker network create --driver bridge net_proxy
 echo -e "${GRA}=== Done ===${NC}"
 
-echo -e "\n${GRA}=== Creating YAML File for Porxy and starting Container ===${NC}"
+echo -e "\n${GRA}=== Creating YAML File for Proxy and starting Container ===${NC}"
 cp ./apps_compose.yml /opt/$DOMAIN/apps_compose.yml
 sed -i -e 's/DOMAIN/'"$DOMAIN"'/g' /opt/$DOMAIN/apps_compose.yml
 docker-compose -f /opt/$DOMAIN/apps_compose.yml up -d
@@ -57,6 +64,26 @@ echo -e "${GRA}=== Done ===${NC}"
 
 
 
+
+
+
+
+
+# Installation Shell in a Box
+clear
+echo -e "${YEL}=== Installing Shell in a Box shell."$DOMAIN" ===${NC}"
+
+echo -e "\n${GRA}=== Creating Bind-Volumes Folders ===${NC}"
+echo -e "${GRA}=== Done ===${NC}"
+
+echo -e "\n${GRA}=== Creating YAML File for Porxy and starting Container ===${NC}"
+cp ./shell_compose.yml /opt/$DOMAIN/shell_compose.yml
+sed -i -e 's/DOMAIN/'"$DOMAIN"'/g' /opt/$DOMAIN/shell_compose.yml
+sed -i -e 's/SIABUSER/'"$SIABUSER"'/g' /opt/$DOMAIN/shell_compose.yml
+sed -i -e 's/SIABPASSWORD/'"$SIABPASSWORD"'/g' /opt/$DOMAIN/shell_compose.yml
+sed -i -e 's/LETSENCRYPTEMAIL/'"$LETSENCRYPTEMAIL"'/g' /opt/$DOMAIN/shell_compose.yml
+docker-compose -f /opt/$DOMAIN/shell_compose.yml up -d
+echo -e "${GRA}=== Done ===${NC}"
 
 
 
