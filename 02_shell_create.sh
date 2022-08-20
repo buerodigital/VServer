0af1f605cf2356b368bfc14d95667c64f1519a69
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-20 20:34:42 +0200

    Create 02_shell_create.sh

---
+++ 02_shell_create.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+source include.sh
+
+# Installation Shell in a Box
+
+clear
+echo -e "${YEL}=== Installing Shell in a Box shell."$DOMAIN" ===${NC}"
+
+echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
+mkdir $WORKFOLDER/02_shell
+echo -e "${GRA}=== Done ===${NC}"
+
+echo -e "\n${GRA}=== Creating YAML File for Landing Page and starting Container ===${NC}"
+cp /opt/VServer/02_shell_compose.yml $WORKFOLDER/02_shell/02_shell_compose.yml
+sed -i -e "s|SIABUSER|$SIABUSER|g" $WORKFOLDER/02_shell/02_shell_compose.yml
+sed -i -e "s|SIABPASSWORD|$SIABPASSWORD|g" $WORKFOLDER/02_shell/02_shell_compose.yml
+sed -i -e "s|DOMAIN|$DOMAIN|g" $WORKFOLDER/02_shell/02_shell_compose.yml
+sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WORKFOLDER/02_shell/02_shell_compose.yml
+docker-compose -f $WORKFOLDER/02_shell/02_shell_compose.yml up -d
+echo -e "${GRA}=== Done ===${NC}"
