408774bf30e86070c901498960a2ba59346d8cb9
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-20 20:12:45 +0200

    Update 00_proxy_create.sh

--- 00_proxy_create.sh
+++ 00_proxy_create.sh
@@ -7,11 +7,11 @@ clear
 echo -e "${YEL}=== Installing NGINX-Proxy "$DOMAIN" ===${NC}"
 
 echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
-mkdir $WORKFOLDER/volumes/00_proxy_certs
-mkdir $WORKFOLDER/volumes/00_proxy_conf
-mkdir $WORKFOLDER/volumes/00_proxy_vhost
-mkdir $WORKFOLDER/volumes/00_proxy_dhparam
-mkdir $WORKFOLDER/volumes/00_proxy_html
+mkdir $WORKFOLDER/00_proxy/certs
+mkdir $WORKFOLDER/00_proxy/conf
+mkdir $WORKFOLDER/00_proxy/vhost
+mkdir $WORKFOLDER/00_proxy/dhparam
+mkdir $WORKFOLDER/00_proxy/html
 echo -e "${GRA}=== Done ===${NC}"
 
 echo -e "\n${GRA}=== Creating Proxy Network ===${NC}"
@@ -19,7 +19,7 @@ docker network create --driver bridge net_proxy
 echo -e "${GRA}=== Done ===${NC}"
 
 echo -e "\n${GRA}=== Creating YAML File for Proxy and starting Container ===${NC}"
-cp /opt/VServer/00_proxy_compose.yml $WORKFOLDER/00_proxy_compose.yml
-sed -i -e "s|WORKFOLDER|$WORKFOLDER|g" $WORKFOLDER/00_proxy_compose.yml
-docker-compose -f $WORKFOLDER/00_proxy_compose.yml up -d --remove-orphan
+cp /opt/VServer/00_proxy_compose.yml $WORKFOLDER/00_proxy/00_proxy_compose.yml
+sed -i -e "s|WORKFOLDER|$WORKFOLDER|g" $WORKFOLDER/00_proxy/00_proxy_compose.yml
+docker-compose -f $WORKFOLDER/00_proxy/00_proxy_compose.yml up -d
 echo -e "${GRA}=== Done ===${NC}"
