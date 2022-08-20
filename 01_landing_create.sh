260e506901236b7217cf2ec27f51f89fa489d7d5
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-20 20:16:45 +0200

    Update 01_landing_create.sh

--- 01_landing_create.sh
+++ 01_landing_create.sh
@@ -8,13 +8,18 @@ clear
 echo -e "${YEL}=== Installing Landingpage "$DOMAIN" ===${NC}"
 
 echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
-mkdir $WORKFOLDER/volumes/01_landing_html
+mkdir $WORKFOLDER/01_landing
+mkdir $WORKFOLDER/01_landing/html
 echo -e "${GRA}=== Done ===${NC}"
 
 echo -e "\n${GRA}=== Creating YAML File for Landing Page and starting Container ===${NC}"
-cp /opt/VServer/01_landing_compose.yml $WORKFOLDER/01_landing_compose.yml
-sed -i -e "s|WORKFOLDER|$WORKFOLDER|g" $WORKFOLDER/01_landing_compose.yml
-sed -i -e "s|DOMAIN|$DOMAIN|g" $WORKFOLDER/01_landing_compose.yml
-sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WORKFOLDER/01_landing_compose.yml
-docker-compose -f $WORKFOLDER/01_landing_compose.yml up -d --remove-orphan
+cp /opt/VServer/01_landing_compose.yml $WORKFOLDER/01_landing/01_landing_compose.yml
+sed -i -e "s|WORKFOLDER|$WORKFOLDER|g" $WORKFOLDER/01_landing/01_landing_compose.yml
+sed -i -e "s|DOMAIN|$DOMAIN|g" $WORKFOLDER/01_landing/01_landing_compose.yml
+sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WORKFOLDER/01_landing/01_landing_compose.yml
+docker-compose -f $WORKFOLDER/01_landing/01_landing_compose.yml up -d
+echo -e "${GRA}=== Done ===${NC}"
+
+echo -e "\n${GRA}=== Creating index.html ===${NC}"
+cp /opt_VServer/02_landing_index.html $WORKFOLDER/01_landing/html/index.html
 echo -e "${GRA}=== Done ===${NC}"
