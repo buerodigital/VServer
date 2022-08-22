55ac667d757b4b52c1ed6ad59cbd36f1faf19476
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-22 09:38:59 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+                                                                                                                                                                                                                   
+clear                                                                                                                                                                                                              
+
+echo -e "=== Menü - Bitte treffen Sie Ihre Auswahl: ===\n"
