f72eed0a222ea291da6dcc8abcdfec188ddf2e83
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-22 09:46:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-                                                                                                                                                                                                                   
+
+source include.sh
+
 clear                                                                                                                                                                                                              
 
-echo -e "=== Menü - Bitte treffen Sie Ihre Auswahl: ===\n"
+echo -e "${PUR}=== Menü - Bitte treffen Sie Ihre Auswahl: ===\n${NC}"
+
+echo -e "${PUR} S00 - Systemupdate ${NC}"
+echo -e "${PUR} S01 - Einrichtung Grundsystem ${NC}"
+echo -e "${PUR} S02 - Backup ${NC}"
+echo -e "${PUR} S03 - Restore ${NC}"
+
+echo -e "${PUR} 00 - Proxy ${NC}"
+echo -e "${PUR} 01 - Landingpage ${NC}"
+echo -e "${PUR} 02 - Shell ${NC}"
+echo -e "${PUR} 03 - OnlyOffice fpr Nextcloud ${NC}"
+echo -e "${PUR} 04 - High Performance Signaling Server for Nextcloud${NC}"
+echo -e "${PUR} 05 - Redirect (statt 01 Landingpage)${NC}"
