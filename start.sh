eb4866968b0bdd549a19dbf2cea3e07757a0d8ff
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-22 09:51:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,3 +17,30 @@ echo -e "${PUR} 02 - Shell ${NC}"
 echo -e "${PUR} 03 - OnlyOffice fpr Nextcloud ${NC}"
 echo -e "${PUR} 04 - High Performance Signaling Server for Nextcloud${NC}"
 echo -e "${PUR} 05 - Redirect (statt 01 Landingpage)${NC}"
+
+
+PS3="Select the operation: "
+
+select opt in add subtract multiply divide quit; do
+
+  case $opt in
+    add)
+      read -p "Enter the first number: " n1
+      ;;
+    subtract)
+      read -p "Enter the first number: " n1
+      ;;
+    multiply)
+      read -p "Enter the first number: " n1
+      ;;
+    divide)
+      read -p "Enter the first number: " n1
+      ;;
+    quit)
+      break
+      ;;
+    *) 
+      echo "Invalid option $REPLY"
+      ;;
+  esac
+done
