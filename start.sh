4040b2929eeba3ceb95e99a95db341631ea7b480
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-23 21:44:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+#fuck
 source include.sh
 
 clear                                                                                                                                                                                                              
