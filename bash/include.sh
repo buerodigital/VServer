4935e34820dd365ed1c087177caddb072d27901d
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-24 21:27:56 +0200

    Create include.sh

---
+++ bash/include.sh
@@ -0,0 +1,8 @@
+
+# Farbedefinitionen
+
+NC='\033[31;0m'       # no colors or formatting
+RED='\033[0;31;1m'    # print text in bold red
+PUR='\033[0;35;1m'    # print text in bold purple
+YEL='\033[0;33;1m'    # print text in bold Yellow
+GRA='\033[0;37;1m'    # print text in bold Gray
