daa599e0849a50a87f02ba2d668ae7462904420b
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-25 21:21:23 +0200

    Update requirements.sh

--- bash/requirements.sh
+++ bash/requirements.sh
@@ -3,7 +3,10 @@
 clear                                                                                                                                                                                                              
 
 echo -e "=== Installing required Software ==="
-sudo apt-get install docker docker-compose git curlftpfs ufw                                                                                                                                                                                                  
+sudo apt-get install docker docker-compose git curlftpfs ufw  
+
+echo -e "=== Creating Backup Folder ==="
+sudo mkdir /opt/VServer_backup
 
                                                                                                                                                                                                                    
 echo -e "\n=== Done ==="
