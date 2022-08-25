a5554e99b5f7b9e5b6ab23088d02eae39aeb30fb
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-25 13:26:24 +0200

    Update requirements.sh

--- bash/requirements.sh
+++ bash/requirements.sh
@@ -3,7 +3,7 @@
 clear                                                                                                                                                                                                              
 
 echo -e "=== Installing required Software ==="
-sudo apt-get install docker docker-compose git curlftpfs                                                                                                                                                                                                  
+sudo apt-get install docker docker-compose git curlftpfs ufw                                                                                                                                                                                                  
 
                                                                                                                                                                                                                    
 echo -e "\n=== Done ==="
