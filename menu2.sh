72011259aa291c780e81dee7052d792c2b705dc9
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-25 08:34:23 +0200

    Update menu2.sh

--- menu2.sh
+++ menu2.sh
@@ -14,12 +14,12 @@ compactbutton=,white
 funct_main () {
 CHOICE=$(
 whiptail --title "Tileltext" --menu "Make your choice" --cancel-button "Beenden" 16 100 9 \
-        "1)" "Submenu 1"   \
-        "2)" "Submenu 2"  \
+        "1)" "Submenu 1" \
+        "2)" "Submenu 2" \
         "3)" "Submenu 3" \
         "4)" "Submenu 4" \
         "5)" "Submenu 5" \
-        "6)" "Submenu 6" \\
+        "6)" "Submenu 6" \
         "q)" "quit"  3>&2 2>&1 1>&3                                                                                                                                                                                                                 
 )
                                                                                                                                                                                                                    
@@ -28,7 +28,7 @@ result=$(whoami)
 case $CHOICE in
         "1)")   
                 result="Sub1"
-                funct_main
+                funct_sub1
         ;;
         "2)")   
                 result="Sub2"
@@ -55,5 +55,55 @@ case $CHOICE in
         ;;
 esac
 }
+
+funct_sub1 () {
+CHOICE=$(
+whiptail --title "Tileltext" --menu "Make your choice" --cancel-button "Beenden" 16 100 9 \
+        "1)" "Doing 1" \
+        "2)" "Doing 2" \
+        "3)" "Doing 3" \
+        "4)" "Doing 4" \
+        "5)" "Doing 5" \
+        "6)" "Doing 6" \
+        "b)" "zurück" \
+        "q)" "quit"  3>&2 2>&1 1>&3  
+)
+                                                                                                                                                                                                                   
+result=$(whoami)
+                                                                                                                                                                                                                   
+case $CHOICE in
+        "1)")   
+                result="Do1"
+                funct_sub1
+        ;;
+        "2)")   
+                result="Do2"
+                funct_sub1
+        ;;
+        "3)")   
+                result="Do3"
+                funct_sub1
+        ;;
+        "4)")
+                result="Do4"
+                funct_sub1
+        ;;
+        "5)")
+                result="Do5"
+                funct_sub1
+        ;;
+        "6)")
+                result="Do6"
+                funct_sub1
+        ;;
+        "b)") 
+                funct_main
+        ;;
+        "q)") 
+                exit
+        ;;
+esac
+}
+
 
 funct_main
