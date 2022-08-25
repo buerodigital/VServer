18263d60e07873372bc9b7e1a8afcb0eec434cb0
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-25 13:24:35 +0200

    Create requirements.sh

---
+++ bash/requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+                                                                                                                                                                                                                   
+clear                                                                                                                                                                                                              
+
+echo -e "=== Installing required Software ==="
+sudo apt-get install docker docker-compose git curlftpfs                                                                                                                                                                                                  
+
+                                                                                                                                                                                                                   
+echo -e "\n=== Done ==="
+
+read -n1 -rp "Press any key to continue" key
