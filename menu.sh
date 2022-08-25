55963e270965ca7ed70ebb8c0a55367a49eb8f97
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-25 20:41:12 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -55,13 +55,13 @@ case $CHOICE in
 		LETSENCRYPTEMAIL2=$(whiptail --inputbox "email Adresse für Lets Encrypt (aktuell "$LETSENCRYPTEMAIL"):" 16 100 3>&1 1>&2 2>&3)
 		sed -i -e "s|$LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL2|g" ./VServer/bash/config.sh
 		
-		FTP_URL=$(whiptail --inputbox "Domainname Server (aktuell "$FTP_URL"):" 16 100 3>&1 1>&2 2>&3)
+		FTP_URL2=$(whiptail --inputbox "Domainname Server (aktuell "$FTP_URL"):" 16 100 3>&1 1>&2 2>&3)
 		sed -i -e "s|$FTP_URL|$FTP_URL2|g" ./VServer/bash/config.sh
 		
 		FTP_USER2=$(whiptail --inputbox "Domainname Server (aktuell "$FTP_USER"):" 16 100 3>&1 1>&2 2>&3)
 		sed -i -e "s|$FTP_USER|$FTP_USER2|g" ./VServer/bash/config.sh
 		
-		FTP_PASS=$(whiptail --inputbox "Domainname Server (aktuell "$FTP_PASS"):" 16 100 3>&1 1>&2 2>&3)
+		FTP_PASS2=$(whiptail --inputbox "Domainname Server (aktuell "$FTP_PASS"):" 16 100 3>&1 1>&2 2>&3)
 		sed -i -e "s|$FTP_PASS|$FTP_PASS2|g" ./VServer/bash/config.sh
 		;;
 	"3)")
