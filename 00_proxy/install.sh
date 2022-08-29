bc1816f2ac3b0279e786a1f763c12b0a7c62a756
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 19:59:27 +0200

    Update install.sh

--- 00_proxy/install.sh
+++ 00_proxy/install.sh
@@ -7,12 +7,11 @@ clear
 echo -e "${YEL}=== Installing NGINX-Proxy "$DOMAIN" ===${NC}"
 
 echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
-mkdir $WORKFOLDER/00_proxy
-mkdir $WORKFOLDER/00_proxy/certs
-mkdir $WORKFOLDER/00_proxy/conf
-mkdir $WORKFOLDER/00_proxy/vhost
-mkdir $WORKFOLDER/00_proxy/dhparam
-mkdir $WORKFOLDER/00_proxy/html
+mkdir /opt/VServer/00_proxy/certs
+mkdir /opt/VServer/00_proxy/conf
+mkdir /opt/VServer/00_proxy/vhost
+mkdir /opt/VServer/00_proxy/dhparam
+mkdir /opt/VServer/00_proxy/html
 echo -e "${GRA}=== Done ===${NC}"
 
 echo -e "\n${GRA}=== Creating Proxy Network ===${NC}"
@@ -20,7 +19,7 @@ docker network create --driver bridge net_proxy
 echo -e "${GRA}=== Done ===${NC}"
 
 echo -e "\n${GRA}=== Creating YAML File for Proxy and starting Container ===${NC}"
-cp /opt/VServer/00_proxy_compose.yml $WORKFOLDER/00_proxy/00_proxy_compose.yml
-sed -i -e "s|WORKFOLDER|$WORKFOLDER|g" $WORKFOLDER/00_proxy/00_proxy_compose.yml
 docker-compose -f $WORKFOLDER/00_proxy/00_proxy_compose.yml up -d
 echo -e "${GRA}=== Done ===${NC}"
+
+
