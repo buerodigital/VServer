faffe86420ffb7c13263448d3b9b389174bc65e3
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 20:20:25 +0200

    Update install.sh

--- 05_redirect/install.sh
+++ 05_redirect/install.sh
@@ -1,19 +1,16 @@
 
 #!/bin/bash
 
-source include.sh
+source /opt/VServer/bash/include.sh
+source /opt/VServer/05_redirect/config.sh
 
 # Installation Redirect for nginx Proxy
 
 clear
 echo -e "${YEL}=== Installing Redirect for nginx Proxy "$DOMAIN" ===${NC}"
 
-echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
-mkdir $WORKFOLDER/05_redirect
-echo -e "${GRA}=== Done ===${NC}"
-
 echo -e "\n${GRA}=== Creating YAML File for Landing Page and starting Container ===${NC}"
-cp /opt/VServer/05_redirect_compose.yml $WORKFOLDER/05_redirect/05_redirect_compose.yml
-sed -i -e "s|DOMAIN|$DOMAIN|g" $WORKFOLDER/05_redirect/05_redirect_compose.yml
-docker-compose -f $WORKFOLDER/05_redirect/05_redirect_compose.yml up -d
+sed -i -e "s|host.domain.com|$REDIRECT_HOST|g" /opt/VServer/05_redirect/docker-compose.yml
+sed -i -e "s|redirect.domain.com|$REDIRECT_DOMAIN|g" /opt/VServer/05_redirect/docker-compose.yml
+docker-compose -f /opt/VServer/05_redirect/docker-compose.yml up -d
 echo -e "${GRA}=== Done ===${NC}"
