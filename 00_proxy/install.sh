ae073b4427a0baa4477c641025180313da00689f
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 21:40:51 +0200

    Update install.sh

--- 00_proxy/install.sh
+++ 00_proxy/install.sh
@@ -3,25 +3,25 @@
 clear
 source /opt/VServer/bash/include.sh
 
-echo -e "${YEL}=== Installing NGINX-Proxy "$DOMAIN" ===${NC}"
+echo -en "${YEL}=== Installing NGINX-Proxy "$DOMAIN" ===${NC}"
 
-echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
+echo -en "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
 mkdir /opt/VServer/00_proxy/certs
 mkdir /opt/VServer/00_proxy/conf
 mkdir /opt/VServer/00_proxy/vhost
 mkdir /opt/VServer/00_proxy/dhparam
 mkdir /opt/VServer/00_proxy/html
-echo -e "${GRA}=== Done ===${NC}"
+echo -en "${GRA}=== Done ===${NC}"
 
-echo -e "\n${GRA}=== Creating Proxy Network ===${NC}"
+echo -en "\n${GRA}=== Creating Proxy Network ===${NC}"
 docker network create --driver bridge net_proxy
-echo -e "${GRA}=== Done ===${NC}"
+echo -en "${GRA}=== Done ===${NC}"
 
-echo -e "\n${GRA}=== Creating YAML File and starting Container ===${NC}"
+echo -en "\n${GRA}=== Creating YAML File and starting Container ===${NC}"
 docker-compose -f /opt/VServer/00_proxy/docker-compose.yml up -d
-echo -e "${GRA}=== Done ===${NC}"
+echo -en "${GRA}=== Done ===${NC}"
 
-#echo -e "\n${GRA}=== Creating *** ===${NC}"
-#echo -e "${GRA}=== Done ===${NC}"
+#echo -en "\n${GRA}=== Creating *** ===${NC}"
+#echo -en "${GRA}=== Done ===${NC}"
 
 
