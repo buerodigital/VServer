336b9aeacef93e9cc76ef47be17cd494bd327cd5
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 21:31:14 +0200

    Update install.sh

--- 01_landing/install.sh
+++ 01_landing/install.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-source include.sh
-
-# Installation Landingpage
-
 clear
+source /opt/VServer/bash/include.sh
+source /opt/VServer/bash/config.sh
+
 echo -e "${YEL}=== Installing Landingpage "$DOMAIN" ===${NC}"
 
-echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
-mkdir $WORKFOLDER/01_landing
-mkdir $WORKFOLDER/01_landing/html
-echo -e "${GRA}=== Done ===${NC}"
+#echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
+#echo -e "${GRA}=== Done ===${NC}"
+
+#echo -e "\n${GRA}=== Creating Proxy Network ===${NC}"
+#echo -e "${GRA}=== Done ===${NC}"
 
 echo -e "\n${GRA}=== Creating YAML File for Landing Page and starting Container ===${NC}"
-cp /opt/VServer/01_landing_compose.yml $WORKFOLDER/01_landing/01_landing_compose.yml
-sed -i -e "s|WORKFOLDER|$WORKFOLDER|g" $WORKFOLDER/01_landing/01_landing_compose.yml
 sed -i -e "s|DOMAIN|$DOMAIN|g" $WORKFOLDER/01_landing/01_landing_compose.yml
 sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WORKFOLDER/01_landing/01_landing_compose.yml
-docker-compose -f $WORKFOLDER/01_landing/01_landing_compose.yml up -d
+docker-compose -f /opt/VServer/01_landing/docker-compose.yml up -d
 echo -e "${GRA}=== Done ===${NC}"
 
-echo -e "\n${GRA}=== Creating index.html ===${NC}"
-cp /opt_VServer/01_landing_index.html $WORKFOLDER/01_landing/html/index.html
-echo -e "${GRA}=== Done ===${NC}"
+#echo -e "\n${GRA}=== Creating *** ===${NC}"
+#echo -e "${GRA}=== Done ===${NC}"
