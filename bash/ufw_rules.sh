7c201c96d56687b344237e9e141cdc5abc4a0838
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 21:47:51 +0200

    Update ufw_rules.sh

--- bash/ufw_rules.sh
+++ bash/ufw_rules.sh
@@ -5,15 +5,15 @@ source /opt/VServer/bash/include.sh
 
 echo -en "${YEL}=== Updating Firewall rules ===${NC}"
 
-echo -en "\n${GRA}=== Clearing current rules ===${NC}"
+echo -en "\n${GRE}=== Clearing current rules ===${NC}"
 ufw disable
 yes | ufw reset
 
-echo -en "\n${GRA}=== Adding default Rules ===${NC}"
+echo -en "\n${GRE}=== Adding default Rules ===${NC}"
 ufw default deny incoming
 ufw default allow outgoing
 
-echo -en "\n${GRA}=== Adding External Rules ===${NC}"
+echo -en "\n${GRE}=== Adding External Rules ===${NC}"
 #Anpassen
 ufw allow 22 comment "SSH"
 ufw allow 80 comment "HTTP"
@@ -21,7 +21,7 @@ ufw allow 443 comment "HTTPS"
 ufw allow 3478/tcp comment "COTURN"
 ufw allow 3478/udp comment "COTURN"
 
-echo -en "\n${GRA}=== Enabling Firewall ===${NC}"
+echo -en "\n${GRE}=== Enabling Firewall ===${NC}"
 yes | ufw enable
 ufw status verbose
 
