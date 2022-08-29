48354fd783a799a4dd0a0184a1264f4da8ff3fed
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 21:22:47 +0200

    Update ufw_rules.sh

--- bash/ufw_rules.sh
+++ bash/ufw_rules.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-echo "Updating Firewall rules"
-echo -e "=====\n"
+clear
 
-echo "Clearing current rules"
-echo -e "=====\n"
+echo "${YEL}=== Updating Firewall rules ===${NC}"
+
+echo "\n${GRA}=== Clearing current rules ===${NC}"
 ufw disable
 yes | ufw reset
 
-echo "Adding default Rules"
-echo -e "=====\n"
+echo "\n${GRA}=== Adding default Rules ===${NC}"
 ufw default deny incoming
 ufw default allow outgoing
 
-echo "Adding External Rules"
-echo -e "=====\n"
-
+echo "\n${GRA}=== Adding External Rules ===${NC}"
 #Anpassen
 ufw allow 22 comment "SSH"
 ufw allow 80 comment "HTTP"
@@ -23,10 +20,9 @@ ufw allow 443 comment "HTTPS"
 ufw allow 3478/tcp comment "COTURN"
 ufw allow 3478/udp comment "COTURN"
 
-echo "Enabling Firewall"
-echo -e "=====\n"
+echo "\n${GRA}=== Enabling Firewall ===${NC}"
 yes | ufw enable
 ufw status verbose
 
-echo "Done"
-echo -e "=====\n"
+echo "\n${YEL}=== Done ===${NC}\n"
+read -n1 -rp "Press any key to continue" key
