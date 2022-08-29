0536b85a61ecac26c9c1f15df9ea8458fe25a049
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 21:50:14 +0200

    Update ufw_rules.sh

--- bash/ufw_rules.sh
+++ bash/ufw_rules.sh
@@ -3,17 +3,17 @@
 clear
 source /opt/VServer/bash/include.sh
 
-echo -en "${YEL}=== Updating Firewall rules ===${NC}"
+echo -e "${YEL}=== Updating Firewall rules ===${NC}"
 
-echo -en "\n${GRE}=== Clearing current rules ===${NC}"
+echo -e "\n${GRE}=== Clearing current rules ===${NC}"
 ufw disable
 yes | ufw reset
 
-echo -en "\n${GRE}=== Adding default Rules ===${NC}"
+echo -e "\n${GRE}=== Adding default Rules ===${NC}"
 ufw default deny incoming
 ufw default allow outgoing
 
-echo -en "\n${GRE}=== Adding External Rules ===${NC}"
+echo -e "\n${GRE}=== Adding External Rules ===${NC}"
 #Anpassen
 ufw allow 22 comment "SSH"
 ufw allow 80 comment "HTTP"
@@ -21,9 +21,9 @@ ufw allow 443 comment "HTTPS"
 ufw allow 3478/tcp comment "COTURN"
 ufw allow 3478/udp comment "COTURN"
 
-echo -en "\n${GRE}=== Enabling Firewall ===${NC}"
+echo -e "\n${GRE}=== Enabling Firewall ===${NC}"
 yes | ufw enable
 ufw status verbose
 
-echo -en "\n${YEL}=== Done ===${NC}\n"
+echo -e "\n${YEL}=== Done ===${NC}\n"
 read -n1 -rp "Press any key to continue" key
