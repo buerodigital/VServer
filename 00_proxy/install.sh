15501d866372e0dc009b4935e7e64c551965ff8e
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 21:30:55 +0200

    Update install.sh

--- 00_proxy/install.sh
+++ 00_proxy/install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-source /opt/VServer/bash/include.sh
 clear
-
+source /opt/VServer/bash/include.sh
 
 echo -e "${YEL}=== Installing NGINX-Proxy "$DOMAIN" ===${NC}"
-
 
 echo -e "\n${GRA}=== Creating Bind-Volume Folders ===${NC}"
 mkdir /opt/VServer/00_proxy/certs
@@ -15,16 +13,13 @@ mkdir /opt/VServer/00_proxy/dhparam
 mkdir /opt/VServer/00_proxy/html
 echo -e "${GRA}=== Done ===${NC}"
 
-
 echo -e "\n${GRA}=== Creating Proxy Network ===${NC}"
 docker network create --driver bridge net_proxy
 echo -e "${GRA}=== Done ===${NC}"
 
-
 echo -e "\n${GRA}=== Creating YAML File and starting Container ===${NC}"
 docker-compose -f /opt/VServer/00_proxy/docker-compose.yml up -d
 echo -e "${GRA}=== Done ===${NC}"
-
 
 #echo -e "\n${GRA}=== Creating *** ===${NC}"
 #echo -e "${GRA}=== Done ===${NC}"
