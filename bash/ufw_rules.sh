ecb3d40b458dcd2b39d05046ddc2412f8bcbd8d8
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 21:40:58 +0200

    Update ufw_rules.sh

--- bash/ufw_rules.sh
+++ bash/ufw_rules.sh
@@ -2,17 +2,17 @@
 
 clear
 
-echo "${YEL}=== Updating Firewall rules ===${NC}"
+echo -en "${YEL}=== Updating Firewall rules ===${NC}"
 
-echo "\n${GRA}=== Clearing current rules ===${NC}"
+echo -en "\n${GRA}=== Clearing current rules ===${NC}"
 ufw disable
 yes | ufw reset
 
-echo "\n${GRA}=== Adding default Rules ===${NC}"
+echo -en "\n${GRA}=== Adding default Rules ===${NC}"
 ufw default deny incoming
 ufw default allow outgoing
 
-echo "\n${GRA}=== Adding External Rules ===${NC}"
+echo -en "\n${GRA}=== Adding External Rules ===${NC}"
 #Anpassen
 ufw allow 22 comment "SSH"
 ufw allow 80 comment "HTTP"
@@ -20,9 +20,9 @@ ufw allow 443 comment "HTTPS"
 ufw allow 3478/tcp comment "COTURN"
 ufw allow 3478/udp comment "COTURN"
 
-echo "\n${GRA}=== Enabling Firewall ===${NC}"
+echo -en "\n${GRA}=== Enabling Firewall ===${NC}"
 yes | ufw enable
 ufw status verbose
 
-echo "\n${YEL}=== Done ===${NC}\n"
+echo -en "\n${YEL}=== Done ===${NC}\n"
 read -n1 -rp "Press any key to continue" key
