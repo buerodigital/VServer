d6062b4f6d88d64fccae46f9e3be9ebad41404ca
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-29 21:50:10 +0200

    Update include.sh

--- bash/include.sh
+++ bash/include.sh
@@ -6,4 +6,4 @@ RED='\033[0;31;1m'    # print text in bold red
 PUR='\033[0;35;1m'    # print text in bold purple
 YEL='\033[0;33;1m'    # print text in bold Yellow
 GRA='\033[0;37;1m'    # print text in bold Gray
-GRE="\[\033[1;32m\]"  # Green
+GRE='\033[1;32m'  # Green
