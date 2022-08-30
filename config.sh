740e91d68ff8526eebd0523cb86ab9f1a444c3a7
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-30 15:53:10 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
-#Allgemein
+#Allgemeine Konfiguration
 DOMAIN=test.domain.com
 LETSENCRYPTEMAIL=domainmail.maildomain.com
 
-#Zugangsdaten FTP Server
+#Zugangsdaten FTP Server (Backup)
 FTP_URL=ftp.domain.com
 FTP_USER=user
 FTP_PASS=pass
