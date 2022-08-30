3f0a28dba0a74887ade050b66853b84890ea5ee7
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-30 21:46:03 +0200

    Update include.sh

--- include.sh
+++ include.sh
@@ -1,9 +1,21 @@
 
-# Reset
-NC='\033[0m'       # Text Reset
+# Allgemeine Konfiguration
+DOMAIN=test.domain.com
+LETSENCRYPTEMAIL=domainmail.maildomain.com
 
-# Regular Colors
-RED='\033[0;31m'    # Red
+
+# Zugangsdaten FTP Server (Backup)
+FTP_URL=ftp.domain.com
+FTP_USER=ftp_user
+FTP_PASS=ftp_pass
+
+
+
+
+
+# Farben
+NC='\033[0m'       # Text Reset
+RED='\033[0;31m'   # Red
 GRE='\033[0;32m'   # Green
 YEL='\033[0;33m'   # Yellow
 BLU='\033[0;34m'   # Blue
