d830bd06f0387fc4c05b7e2767f5b11f98ea4010
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-30 16:07:04 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,20 +3,20 @@
 clear
 source include.sh
 
-echo -en "${YEL}=== System Update ===${NC}"
+echo -e "${YEL}=== System Update ===${NC}"
 
-echo -en "\n${GRA}=== Updating ===${NC}"                                                                                                                                                                                                      
+echo -e "\n${GRA}=== Updating ===${NC}"                                                                                                                                                                                                      
 sudo apt update                                                                                                                                                                                                    
 
-echo -en "\n${GRA}=== Upgrading ===${NC}"
+echo -e "\n${GRA}=== Upgrading ===${NC}"
 sudo apt full-upgrade -y
 
-echo -en "\n${GRA}=== Cleaning ===${NC}"
+echo -e "\n${GRA}=== Cleaning ===${NC}"
 sudo apt autoremove -y
 sudo apt autoclean -y
 
-echo -en "\n${GRA}=== Rebooting if required ===${NC}"
+echo -e "\n${GRA}=== Rebooting if required ===${NC}"
 [ -f /var/run/reboot-required ] && sudo reboot -f
                                                                                                                                                                                                                    
-echo -en "\n${YEL}=== Done ===${NC}\n"
+echo -e "\n${YEL}=== Done ===${NC}\n"
 read -n1 -rp "Press any key to continue" key
