9f8696444e0c4ae2c347d7196df06839a0dc4e34
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-30 21:46:19 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
+WF="$(dirname "$(readlink -e "$0")")"
 clear
-source include.sh
+source $WF/include.sh 
 
 echo -e "${YEL}=== System Update ===${NC}"
 
 echo -e "\n${GRE}=== Updating ===${NC}"                                                                                                                                                                                                      
-sudo apt update                                                                                                                                                                                                    
+apt update                                                                                                                                                                                                    
 
 echo -e "\n${GRE}=== Upgrading ===${NC}"
-sudo apt full-upgrade -y
+apt full-upgrade -y
 
 echo -e "\n${GRE}=== Cleaning ===${NC}"
-sudo apt autoremove -y
-sudo apt autoclean -y
+apt autoremove -y
+apt autoclean -y
 
 echo -e "\n${GRE}=== Rebooting if required ===${NC}"
-[ -f /var/run/reboot-required ] && sudo reboot -f
+[ -f /var/run/reboot-required ] && reboot -f
                                                                                                                                                                                                                    
 echo -e "\n${YEL}=== Done ===${NC}\n"
 read -n1 -rp "Press any key to continue" key
