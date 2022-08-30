a2791ede39cc66a6b4592da0490a244b7e7758ad
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-30 21:46:12 +0200

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-source include.sh
-
-# Hinweis auf Löschung aller Daten
+WF="$(dirname "$(readlink -e "$0")")"
 clear
-echo -e "${RED}=== Achtung!!! Alle Daten werden gelöscht!!! ==="
-echo -e "=== Ggf. mit CTRL + C abbrechen ==="
-COUNT=5
-while [ $COUNT -gt 0 ]; do
-  echo -e "$COUNT"
-  let COUNT=COUNT-1
-  sleep 1
-done
-echo -e "GO!${NC}"
+source $WF/include.sh 
 
+echo -e "${RED}=== Achtung!!! Alle Daten werden gelöscht!!! ===${NC}"
 
-# Docker zurücksetzen
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -a -q)
-yes | docker system prune -a
+echo -e "\n${RED}=== Löschen bestätigen ===${NC}"                                                                                                                                                                                                      
+read -p "Bitte geben Sie "JA" ein: " VAR_CONFIRM
+
+if VAR_CONFIRM = "JA" then
+
+	# Docker zurücksetzen
+	docker stop $(docker ps -a -q)
+	docker rm $(docker ps -a -q)
+	docker rmi $(docker images -a -q)
+	yes | docker system prune -a
+	
+	#Verzeichnis löschen
+	rm -R $WF
+else
+
+	echo "\n${RED}=== Löschen abgebrochen ===${NC}"
+
+fi
+
+echo -e "\n${RED}=== Done ===${NC}\n"
+read -n1 -rp "Press any key to continue" key
+
 
