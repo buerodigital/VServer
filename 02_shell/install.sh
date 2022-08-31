b9177d75e8b9f04218455ea5a1e9a593efd2aa91
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-31 20:16:17 +0200

    Update install.sh

--- 02_shell/install.sh
+++ 02_shell/install.sh
@@ -11,7 +11,7 @@ echo -e "${YEL}=== Installing Shell in a Box ===${NC}"
 
 #echo -e "\n${GRE}=== Creating Proxy Network ===${NC}"
 #echo -e "${GRE}=== Done ===${NC}"
-
+echo $SHELL_USER
 echo -e "\n${GRE}=== Creating YAML File and starting Container ===${NC}"
 sed -i -e "s|SHELL_USER|$SHELL_USER|g" $WF/docker-compose.yml
 sed -i -e "s|SHELL_PASS|$SHELL_PASS|g" $WF/docker-compose.yml
