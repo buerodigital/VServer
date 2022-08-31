15b2f3ec5c996b3df5b3e28951a95ea2ff4936f9
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-31 20:09:50 +0200

    Update install.sh

--- 02_shell/install.sh
+++ 02_shell/install.sh
@@ -17,7 +17,7 @@ sed -i -e "s|SHELL_USER|$SHELL_USER|g" $WF/docker-compose.yml
 sed -i -e "s|SHELL_PASS|$SHELL_PASS|g" $WF/docker-compose.yml
 sed -i -e "s|SHELL_DOMAIN|$SHELL_SUBDOMAIN"."$DOMAIN|g" $WF/docker-compose.yml
 sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WF/docker-compose.yml
-#docker-compose -f $WF/docker-compose.yml build
+docker-compose -f $WF/docker-compose.yml up -d
 echo -e "${GRE}=== Done ===${NC}"
 
 #echo -e "\n${GRE}=== Creating *** ===${NC}"
