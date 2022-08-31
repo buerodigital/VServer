0f129e0c9ac7b52968c1cb7edd411fa9a33356fe
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-31 16:15:57 +0200

    Update install.sh

--- 01_landing/install.sh
+++ 01_landing/install.sh
@@ -13,9 +13,10 @@ echo -e "${YEL}=== Installing Landingpage "$DOMAIN" ===${NC}"
 #echo -e "${GRE}=== Done ===${NC}"
 
 echo -e "\n${GRE}=== Creating YAML File for Landing Page and starting Container ===${NC}"
-sed -i -e "s|DOMAIN|$DOMAIN|g" /opt/VServer/01_landing/docker-compose.yml
-sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" /opt/VServer/01_landing/docker-compose.yml
-docker-compose -f /opt/VServer/01_landing/docker-compose.yml build
+sed -i -e "s|WORKFOLDER|$WF|g" $WF/01_landing/docker-compose.yml
+sed -i -e "s|DOMAIN|$DOMAIN|g" $WF/01_landing/docker-compose.yml
+sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WF/01_landing/docker-compose.yml
+docker-compose -f $WF/01_landing/docker-compose.yml build
 echo -e "${GRE}=== Done ===${NC}"
 
 #echo -e "\n${GRE}=== Creating *** ===${NC}"
