2604696247ee6f54277f6e794339262c31d3d17f
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-31 19:57:00 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ source $WF/include.sh
 
 echo -e "${YEL}=== System Update ===${NC}"
 
-echo -e "\n${GRE}=== Updating ===${NC}"                                                                                                                                                                                                      
-apt update                                                                                                                                                                                                    
+echo -e "\n${GRE}=== Updating ===${NC}"
+apt update
 
 echo -e "\n${GRE}=== Upgrading ===${NC}"
 apt full-upgrade -y
@@ -18,6 +18,6 @@ apt autoclean -y
 
 echo -e "\n${GRE}=== Rebooting if required ===${NC}"
 [ -f /var/run/reboot-required ] && reboot -f
-                                                                                                                                                                                                                   
+
 echo -e "\n${YEL}=== Done ===${NC}\n"
 read -n1 -rp "Press any key to continue" key
