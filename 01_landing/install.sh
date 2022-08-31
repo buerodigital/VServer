c7bd750766a63f6b7672863df657276a175a2b2c
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-08-31 16:23:12 +0200

    Update install.sh

--- 01_landing/install.sh
+++ 01_landing/install.sh
@@ -2,7 +2,7 @@
 
 WF="$(dirname "$(readlink -e "$0")")"
 clear
-source $WF/include.sh 
+source $WF/../include.sh 
 
 echo -e "${YEL}=== Installing Landingpage "$DOMAIN" ===${NC}"
 
@@ -13,10 +13,10 @@ echo -e "${YEL}=== Installing Landingpage "$DOMAIN" ===${NC}"
 #echo -e "${GRE}=== Done ===${NC}"
 
 echo -e "\n${GRE}=== Creating YAML File for Landing Page and starting Container ===${NC}"
-sed -i -e "s|WORKFOLDER|$WF|g" $WF/01_landing/docker-compose.yml
-sed -i -e "s|DOMAIN|$DOMAIN|g" $WF/01_landing/docker-compose.yml
-sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WF/01_landing/docker-compose.yml
-docker-compose -f $WF/01_landing/docker-compose.yml build
+sed -i -e "s|WORKFOLDER|$WF|g" $WF/docker-compose.yml
+sed -i -e "s|DOMAIN|$DOMAIN|g" $WF/docker-compose.yml
+sed -i -e "s|LETSENCRYPTEMAIL|$LETSENCRYPTEMAIL|g" $WF/docker-compose.yml
+docker-compose -f $WF/docker-compose.yml build
 echo -e "${GRE}=== Done ===${NC}"
 
 #echo -e "\n${GRE}=== Creating *** ===${NC}"
