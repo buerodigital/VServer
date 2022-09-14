aa661cb2bfff4ff830d871e98060b7c16efc0731
buerodigital <77230151+buerodigital@users.noreply.github.com>
2022-09-14 12:50:24 +0200

    Update install.sh

--- 04_coturn/install.sh
+++ 04_coturn/install.sh
@@ -25,20 +25,28 @@ echo -e "${GRE}=== Done ===${NC}"
 #echo -e "${GRE}=== Done ===${NC}"
 
 
-
-echo -e "\n${GRE}=== Creating YAML File for Landing Page and starting Container ===${NC}"
+echo -e "\n${GRE}=== Creating YAML File for Landing Page ===${NC}"
 sed -i -e "s|WORKFOLDER|$WF|g" $WF/docker-compose.yml
 sed -i -e "s|COTURN_DOMAIN|$COTURN_SUBDOMAIN"."$DOMAIN|g" $WF/docker-compose.yml
 sed -i -e "s|SIGNALING_STATIC_SECRET|$SIGNALING_STATIC_SECRET|g" $WF/docker-compose.yml
-
-
-
 echo -e "${GRE}=== Done ===${NC}"
 
 
-
-#echo -e "\n${GRE}=== Creating *** ===${NC}"
+#echo -e "\n${GRE}=== Creating server.conf for Nextcloud High Performance Signaling Server ===${NC}"
+sed -i -e "s|SIGNALING_HASHKEY|$SIGNALING_HASHKEY|g" $WF/server.conf
+sed -i -e "s|SIGNALING_BLOCKKEY|$SIGNALING_BLOCKKEY|g" $WF/server.conf
+sed -i -e "s|COTURN_BACKEND1|$COTURN_BACKEND1|g" $WF/server.conf
+sed -i -e "s|NEXTCLOUD_SHARED_SECRET1|$NEXTCLOUD_SHARED_SECRET1|g" $WF/server.conf
+sed -i -e "s|NEXTCLOUD_API_KEY|$NEXTCLOUD_API_KEY|g" $WF/server.conf
+sed -i -e "s|SIGNALING_STATIC_SECRET|$SIGNALING_STATIC_SECRET|g" $WF/server.conf
+chmod 664 $WF/server.conf
 #echo -e "${GRE}=== Done ===${NC}"
+
+echo -e "\n${GRE}=== Building and run Images ===${NC}"
+docker-compose -f $WF/docker-compose.yml build
+docker-compose -f $WF/docker-compose.yml up -d
+echo -e "${GRE}=== Done ===${NC}"
+
 
 echo -e "\n${YEL}=== Done ===${NC}\n"
 read -n1 -rp "Press any key to continue" key
